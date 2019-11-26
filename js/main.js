4535d9c5acc1707f7d940fd448bc0ee36e26f6f8
Lantz <cnnrlantz@gmail.com>
2019-11-26 17:08:01 -0600

    Navigation & color testing

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = {
+        'edge': 'right',
+    }
+    var instances = M.Sidenav.init(elems, options);
+  });
